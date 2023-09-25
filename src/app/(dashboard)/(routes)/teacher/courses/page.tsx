929dd9497d860523098b69cd3e6e3f2799b5e1e1
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2023-09-25 18:58:40 +0600

    ci: create course page

--- src/app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,3 +1,13 @@
-const CoursesPage = () => <div>CoursesPage</div>;
+import Link from 'next/link';
+
+import { Button } from '@/components/ui/button';
+
+const CoursesPage = () => (
+  <div className='p-6'>
+    <Link href='/teacher/create'>
+      <Button>New Course</Button>
+    </Link>
+  </div>
+);
 
 export default CoursesPage;
