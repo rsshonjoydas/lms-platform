8c5ad53efa30829e36d10601ae6fc1d311ff11b6
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2023-09-25 15:23:00 +0600

    ci: react-hot-toast provider setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { ClerkProvider } from '@clerk/nextjs';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
+import { ToastProvider } from '@/components/providers/toaster-provider';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -15,7 +16,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <ClerkProvider>
       <html lang='en'>
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ToastProvider />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
