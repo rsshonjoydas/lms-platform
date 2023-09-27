55f01de6b4de77297a8b032265920211170fd8f9
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2023-09-28 05:11:39 +0600

    ci: chapter title

---
+++ src/app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -0,0 +1,44 @@
+import { auth } from '@clerk/nextjs';
+import { NextResponse } from 'next/server';
+
+import prismadb from '@/lib/prismadb';
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { courseId: string; chapterId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const { isPublished, ...values } = await req.json();
+
+    if (!userId) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
+
+    const courseOwner = await prismadb.course.findUnique({
+      where: {
+        id: params.courseId,
+        userId,
+      },
+    });
+
+    if (!courseOwner) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
+
+    const chapter = await prismadb.chapter.update({
+      where: {
+        id: params.chapterId,
+        courseId: params.courseId,
+      },
+      data: {
+        ...values,
+      },
+    });
+
+    return NextResponse.json(chapter);
+  } catch (error) {
+    console.log('[CHAPTERS_CHAPTER_ID]', error);
+    return new NextResponse('Internal Server Error', { status: 500 });
+  }
+}
