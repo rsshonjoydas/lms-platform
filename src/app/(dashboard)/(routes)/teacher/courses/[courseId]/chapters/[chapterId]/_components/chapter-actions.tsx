b5cd61c398ad3eb3f09ebc8b340471cb7a7c50d3
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2023-09-28 21:28:02 +0600

    ci: chapter delete

---
+++ src/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import axios from 'axios';
+import { Trash } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import toast from 'react-hot-toast';
+
+import { ConfirmModal } from '@/components/modals/confirm-modal';
+import { Button } from '@/components/ui/button';
+
+interface ChapterActionsProps {
+  disabled: boolean;
+  courseId: string;
+  chapterId: string;
+  isPublished: boolean;
+}
+
+export const ChapterActions = ({
+  disabled,
+  courseId,
+  chapterId,
+  isPublished,
+}: ChapterActionsProps) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  const onDelete = async () => {
+    try {
+      setIsLoading(true);
+
+      await axios.delete(`/api/courses/${courseId}/chapters/${chapterId}`);
+
+      toast.success('Chapter deleted');
+      router.refresh();
+      router.push(`/teacher/courses/${courseId}`);
+    } catch {
+      toast.error('Something went wrong');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className='flex items-center gap-x-2'>
+      <Button disabled={disabled || isLoading} variant='outline' size='sm'>
+        {isPublished ? 'Unpublish' : 'Publish'}
+      </Button>
+      <ConfirmModal onConfirm={onDelete}>
+        <Button size='sm' disabled={isLoading}>
+          <Trash className='h-4 w-4' />
+        </Button>
+      </ConfirmModal>
+    </div>
+  );
+};
--- src/app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ src/app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -6,6 +6,87 @@ import prismadb from '@/lib/prismadb';
 
 const { Video } = new Mux(process.env.MUX_TOKEN_ID!, process.env.MUX_TOKEN_SECRET!);
 
+export async function DELETE(
+  req: Request,
+  { params }: { params: { courseId: string; chapterId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
+
+    const ownCourse = await prismadb.course.findUnique({
+      where: {
+        id: params.courseId,
+        userId,
+      },
+    });
+
+    if (!ownCourse) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
+
+    const chapter = await prismadb.chapter.findUnique({
+      where: {
+        id: params.chapterId,
+        courseId: params.courseId,
+      },
+    });
+
+    if (!chapter) {
+      return new NextResponse('Not Found', { status: 404 });
+    }
+
+    if (chapter.videoUrl) {
+      const existingMuxData = await prismadb.muxData.findFirst({
+        where: {
+          chapterId: params.chapterId,
+        },
+      });
+
+      if (existingMuxData) {
+        await Video.Assets.del(existingMuxData.assetId);
+        await prismadb.muxData.delete({
+          where: {
+            id: existingMuxData.id,
+          },
+        });
+      }
+    }
+
+    const deletedChapter = await prismadb.chapter.delete({
+      where: {
+        id: params.chapterId,
+      },
+    });
+
+    const publishedChaptersInCourse = await prismadb.chapter.findMany({
+      where: {
+        courseId: params.courseId,
+        isPublished: true,
+      },
+    });
+
+    if (!publishedChaptersInCourse.length) {
+      await prismadb.course.update({
+        where: {
+          id: params.courseId,
+        },
+        data: {
+          isPublished: false,
+        },
+      });
+    }
+
+    return NextResponse.json(deletedChapter);
+  } catch (error) {
+    console.log('[CHAPTER_ID_DELETE]', error);
+    return new NextResponse('Internal Server Error', { status: 500 });
+  }
+}
+
 export async function PATCH(
   req: Request,
   { params }: { params: { courseId: string; chapterId: string } }
