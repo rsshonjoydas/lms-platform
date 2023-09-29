fa1df3a0305996dfec35b3918c78758dd7660ec8
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2023-09-29 07:41:06 +0600

    ci: delete chapter

--- src/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -4,11 +4,13 @@ import { CircleDollarSign, LayoutDashboard, ListChecks } from 'lucide-react';
 import { redirect } from 'next/navigation';
 
 // ? Local modules
+import { Banner } from '@/components/banner';
 import { IconBadge } from '@/components/icon-badge';
 import prismadb from '@/lib/prismadb';
 import AttachmentForm from './_components/attachment-form';
 import CategoryForm from './_components/category-form';
 import ChaptersForm from './_components/chapters-form';
+import { CourseActions } from './_components/course-actions';
 import DescriptionForm from './_components/description-form';
 import ImageForm from './_components/image-form';
 import TitleForm from './_components/title-form';
@@ -72,50 +74,62 @@ const CourseIdPage: React.FC<CourseIdPageProps> = async ({ params }) => {
 
     const completionText = `(${completedFields}/${totalFields})`;
 
+    const isComplete = requiredFields.every(Boolean);
+
     return (
-      <div className='p-6'>
-        <div className='flex items-center justify-between'>
-          <div className='flex flex-col gap-y-2'>
-            <h1 className='text-2xl font-medium'>Course setup</h1>
-            <span className='text-sm text-slate-700'>Complete all fields {completionText}</span>
-          </div>
-        </div>
-        <div className='grid grid-cols-1 md:grid-cols-2 gap-6 mt-16'>
-          <div>
-            <div className='flex items-center gap-x-2'>
-              <IconBadge icon={LayoutDashboard} />
-              <h2 className='text-xl'>Customize your course</h2>
+      <>
+        {!course.isPublished && (
+          <Banner label='This course is unpublished. It will not be visible to the students.' />
+        )}
+        <div className='p-6'>
+          <div className='flex items-center justify-between'>
+            <div className='flex flex-col gap-y-2'>
+              <h1 className='text-2xl font-medium'>Course setup</h1>
+              <span className='text-sm text-slate-700'>Complete all fields {completionText}</span>
             </div>
-            <TitleForm initialData={course} courseId={course.id} />
-            <DescriptionForm initialData={course} courseId={course.id} />
-            <ImageForm initialData={course} courseId={course.id} />
-            <CategoryForm
-              initialData={course}
-              courseId={course.id}
-              options={categories.map((category) => ({
-                label: category.name,
-                value: category.id,
-              }))}
+            <CourseActions
+              disabled={!isComplete}
+              courseId={params.courseId}
+              isPublished={course.isPublished}
             />
           </div>
-          <div className='space-y-6'>
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-6 mt-16'>
             <div>
               <div className='flex items-center gap-x-2'>
-                <IconBadge icon={ListChecks} />
-                <h2 className='text-xl'>Course chapters</h2>
+                <IconBadge icon={LayoutDashboard} />
+                <h2 className='text-xl'>Customize your course</h2>
               </div>
-              <ChaptersForm initialData={course} courseId={course.id} />
+              <TitleForm initialData={course} courseId={course.id} />
+              <DescriptionForm initialData={course} courseId={course.id} />
+              <ImageForm initialData={course} courseId={course.id} />
+              <CategoryForm
+                initialData={course}
+                courseId={course.id}
+                options={categories.map((category) => ({
+                  label: category.name,
+                  value: category.id,
+                }))}
+              />
             </div>
-            <div>
-              <div className='flex items-center gap-x-2'>
-                <IconBadge icon={CircleDollarSign} />
-                <h2 className='text-xl'>Sell your course</h2>
+            <div className='space-y-6'>
+              <div>
+                <div className='flex items-center gap-x-2'>
+                  <IconBadge icon={ListChecks} />
+                  <h2 className='text-xl'>Course chapters</h2>
+                </div>
+                <ChaptersForm initialData={course} courseId={course.id} />
               </div>
-              <AttachmentForm initialData={course} courseId={course.id} />
+              <div>
+                <div className='flex items-center gap-x-2'>
+                  <IconBadge icon={CircleDollarSign} />
+                  <h2 className='text-xl'>Sell your course</h2>
+                </div>
+                <AttachmentForm initialData={course} courseId={course.id} />
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </>
     );
   } catch (error) {
     console.error('Error fetching course:', error);
--- src/app/api/courses/[courseId]/route.ts
+++ src/app/api/courses/[courseId]/route.ts
@@ -1,7 +1,55 @@
 import { auth } from '@clerk/nextjs';
+import Mux from '@mux/mux-node';
 import { NextResponse } from 'next/server';
 
 import prismadb from '@/lib/prismadb';
+
+const { Video } = new Mux(process.env.MUX_TOKEN_ID!, process.env.MUX_TOKEN_SECRET!);
+
+export async function DELETE(req: Request, { params }: { params: { courseId: string } }) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
+
+    const course = await prismadb.course.findUnique({
+      where: {
+        id: params.courseId,
+        userId,
+      },
+      include: {
+        chapters: {
+          include: {
+            muxData: true,
+          },
+        },
+      },
+    });
+
+    if (!course) {
+      return new NextResponse('Not Found', { status: 404 });
+    }
+
+    course.chapters.forEach(async (chapter) => {
+      if (chapter.muxData?.assetId) {
+        await Video.Assets.del(chapter.muxData.assetId);
+      }
+    });
+
+    const deleteCourse = await prismadb.course.delete({
+      where: {
+        id: params.courseId,
+      },
+    });
+
+    return NextResponse.json(deleteCourse);
+  } catch (error) {
+    console.log('[COURSE_ID_DELETE]', error);
+    return new NextResponse('Internal Server Error', { status: 500 });
+  }
+}
 
 export async function PATCH(req: Request, { params }: { params: { courseId: string } }) {
   try {
